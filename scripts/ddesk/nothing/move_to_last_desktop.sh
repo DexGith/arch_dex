45113d4372d8b8a0163354b612ee0d42cf984b0f
DexGith <ttdextester@gmail.com>
2025-08-28 00:00:18 +0100

    fixed zsh location so it's ready for git sparse-checkout set

--- scripts/ddesk/nothing/move_to_last_desktop.sh
+++ scripts/ddesk/nothing/move_to_last_desktop.sh
@@ -6,14 +6,22 @@
 # Using the .cache directory is good practice for temporary data.
 LAST_DESKTOP_FILE="$HOME/.cache/last_desktop"
 
-# Check if the file exists and has something in it.
+    
+CURRENT_DESKTOP_FILE="$HOME/.cache/current_desktop"
+
+if [ ! -f "$CURRENT_DESKTOP_FILE" ]; then
+    echo "0" > "$CURRENT_DESKTOP_FILE"
+fi
+
 if [ -s "$LAST_DESKTOP_FILE" ]; then
-    # Read the desktop number from the file.
+
     PREV_DESKTOP=$(cat "$LAST_DESKTOP_FILE")
     
-    # Before we jump, save our CURRENT location so we can come back again.
     xdotool get_desktop > "$LAST_DESKTOP_FILE"
 
     # Go to the previously saved desktop.
     xdotool set_desktop "$PREV_DESKTOP"
+    sleep 0.2
+    xdotool get_desktop > "$CURRENT_DESKTOP_FILE"
+
 fi
---
+++ scripts/ddesk/personal_chat/chat-suggester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WEEK=$(date +%U)
+DATE="$WEEK $(date +"%a, %d at %H:%M")"
+THIS="W$WEEK $DATE"
+
+THE_TEXT="
+
+--------------------
+$THIS
+--------------------
+**Suggester**
+-------------
+
+"
+
+echo "$THE_TEXT" | xclip -se c
+
+sleep 0.1
+xdotool key --clearmodifiers Ctrl+v
+
+
+
+
